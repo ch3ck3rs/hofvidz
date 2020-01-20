7bde8b88f296a5873b627325c3e449c267104b54
rabbit <lvcoffman@gmial.com>
2020-01-19 18:04:10 -0600

    created Hall CRUD pages

--- halls/views.py
+++ halls/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from .models import Hall, Video
 
 
 def home(request):
     return render(request, 'halls/home.html')
+
+
+def dashboard(request):
+    return render(request, 'halls/dashboard.html')
 
 
 class SignUp(generic.CreateView):
@@ -15,3 +19,39 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        user_name, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=user_name, password=password)
+        login(self.request, user)
+        return view
+
+
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
+
+
+class DetailHall(generic.DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'
+
+
+class UpdateHall(generic.UpdateView):
+    model = Hall
+    template_name = 'halls/update_hall.html'
+    fields = ['title']
+    success_url = reverse_lazy('dashboard')
+
+
+class DeleteHall(generic.DeleteView):
+    model = Hall
+    template_name = 'halls/delete_hall.html'
+    success_url = reverse_lazy('dashboard')
